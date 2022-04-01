7f236da3bf28f5c027edcc656d8e9601cf371edf
Eugene Ahn <eugeneahn125@gmail.com>
2022-03-31 20:21:04 -0500

    set up server.js file

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const fs = require("fs")
+const path = require('path');
+const uuid = require("uuid");
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+app.use(express.static("develop/public"))
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "develop/public/notes.html"))
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "develop/public/index.html"))
+})
+
+app.get("/api/notes", (req, res) => {
+    const json = fs.readFileSync("./develop/db/db.json", "utf-8")
+    res.send(json)
+})
+
+app.post("/api/notes", (req, res) => {
+    let json = fs.readFileSync("./develop/db/db.json", "utf-8");
+    let dB = JSON.parse(json);
+    dB.push({ id: uuid.v4(), ...req.body });
+    fs.writeFileSync("./develop/db/db.json", JSON.stringify(dB));
+    console.log(req.body)
+    res.send(JSON.stringify(req.body));
+})
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
